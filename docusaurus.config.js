1ce6cfb187414163fd3ac24601fd1e2b6d230fd7
Your Name <you@example.com>
2023-01-31 19:53:50 +0400

    test moving to docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/docs/",
+          routeBasePath: "/",
 
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
