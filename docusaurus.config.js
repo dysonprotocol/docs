eb89b9c266260f576d7ceeb6c4d9eb44c59e5313
sybil singleton <sybil.singleton@protonmail.com>
2023-04-04 09:57:33 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,13 +43,13 @@ const config = {
 
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://gitlab.com/dysonproject/docs/-/tree/develop/",
+          editUrl: "https://gitlab.com/dysonproject/docs/-/tree/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://gitlab.com/dysonproject/docs/-/tree/develop/",
+          editUrl: "https://gitlab.com/dysonproject/docs/-/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
